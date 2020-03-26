59b343b9d866e4df595f644105806a75926c43ed
mechztcc <dojathan@gmail.com>
2020-03-26 19:10:22 -0300

    created a front end mobile using react native

---
+++ src/pages/Detail/styles.js
@@ -0,0 +1,79 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: 40,
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    incidents: {
+        padding: 24,
+        borderRadius: 18,
+        backgroundColor: '#fff',
+        marginBottom: 16,
+        marginTop: 48
+    },
+    
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold'
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380'
+    },
+
+    contactBox: {
+        padding: 24,
+        borderRadius: 18,
+        backgroundColor: '#fff',
+        marginBottom: 16
+    },
+    
+    heroTitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#13131a',
+        lineHeight: 30
+    },
+
+    heroDescription: {
+        fontSize: 15,
+        color: '#737380',
+        marginTop: 16
+    },
+
+    actions: {
+        marginTop: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    action: {
+        backgroundColor: '#e02041',
+        borderRadius: 8,
+        height: 50,
+        width: '48%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    actionText: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+
+
+})
